cfc7bf9d1e41c281c03282b194eb10f37df7ce1d
Pavel-Surkov <surkovv.pavel@gmail.com>
2023-11-07 16:46:27 +0700

    solve #2.2

--- 2022/#2.2.js
+++ 2022/#2.2.js
@@ -6,20 +6,37 @@ C Z`;
 // Rock Paper Scissors -> ABC -> XYZ -> 1 | 2 | 3 + 0 | 3 | 6
 
 const win = {
+  res: 'win',
   A: 'Y',
   B: 'Z',
   C: 'X',
 };
 
 const lose = {
+  res: 'lose',
   A: 'Z',
   B: 'X',
   C: 'Y',
 };
 
-function calculateScore(enemyVar, myVar) {
-  const winScore =
-    myVar === win[enemyVar] ? 6 : myVar === lose[enemyVar] ? 0 : 3;
+const draw = {
+  res: 'draw',
+  A: 'X',
+  B: 'Y',
+  C: 'Z',
+};
+
+const resultVariants = {
+  X: lose,
+  Y: draw,
+  Z: win,
+};
+
+function calculateScore(enemyVar, res) {
+  const result = resultVariants[res];
+  const myVar = result[enemyVar];
+
+  const winScore = result.res === 'win' ? 6 : result.res === 'lose' ? 0 : 3;
   const rpsScore = myVar === 'X' ? 1 : myVar === 'Y' ? 2 : 3;
 
   return winScore + rpsScore;
