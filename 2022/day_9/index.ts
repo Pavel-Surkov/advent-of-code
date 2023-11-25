5a0747783c57b272b4c38266e6d29e463069b2c1
Pavel-Surkov <surkovv.pavel@gmail.com>
2023-11-25 15:28:15 +0700

    use vectors to know if tail is moving

--- 2022/day_9/index.ts
+++ 2022/day_9/index.ts
@@ -22,97 +22,56 @@ const store = new TailPositionsStore();
 
 type Coords = [number, number];
 
-type DirToPos = {
+type DirToVector = {
   R: Coords;
   L: Coords;
   U: Coords;
   D: Coords;
 };
 
-// axis [x, y]
-const dirToPos: DirToPos = {
+const dirToVector: DirToVector = {
   R: [1, 0],
   L: [-1, 0],
   U: [0, -1],
   D: [0, 1],
 };
 
-const backCases = {
-  L: 'R',
-  R: 'L',
-  U: 'D',
-  D: 'U',
-};
+function isTailMoving(headCoords: Coords, tailCoords: Coords) {
+  let [diffX, diffY] = [
+    Math.abs(headCoords[0] - tailCoords[0]),
+    Math.abs(headCoords[1] - tailCoords[1]),
+  ];
 
+  return diffX <= 1 && diffY <= 1;
+}
+
+// Use vectors to know position of Head and Tail
+// If Head and Tail have a gap => Tail moves to last Head position
+// In this case, add vector position of Tail to Set
 function getTailPositionsCount(arr: string[]) {
-  // axis [x, y]
-  let currentTailCoords: Coords = [0, 0];
+  let headCoords: Coords = [0, 0];
+  let tailCoords: Coords = [0, 0];
 
-  // Set initial coordinates
-  store.addPosition(currentTailCoords.join('_'));
+  arr.forEach((line) => {
+    const direction = line.split(' ')[0] as keyof DirToVector;
+    const steps = +direction.split(' ')[1];
 
-  function move(steps: number, notMoveSteps: number, pos: [number, number]) {
-    // TODO: solve problem: With this function Tail always has position left or right from Head: TH, HT
-    // There different cases: HT, H covers T, H  , etc.
-    // 																					T
-    for (let i = 0 + notMoveSteps; i < steps; i++) {
-      if (i > notMoveSteps) {
-        return;
+    const directionVector = dirToVector[direction];
+
+    for (let i = 0; i < steps; i++) {
+      if (isTailMoving(headCoords, tailCoords)) {
+        headCoords = [
+          headCoords[0] + directionVector[0],
+          headCoords[1] + directionVector[1],
+        ];
+      } else {
       }
-
-      const position: Coords = [
-        currentTailCoords[0] + pos[0],
-        currentTailCoords[1] + pos[1],
-      ];
-
-      currentTailCoords = position;
-
-      store.addPosition(position.join('_'));
-    }
-  }
-
-  arr.reduce((previousLine, line, index) => {
-    const [previousDirection, previousSteps] = previousLine.split(' ');
-    const [direction, steps] = line.split(' ');
-
-    const pos = dirToPos[direction];
-
-    // Case for the first iteration (previousSteps === 0)
-    if (index === 0) {
-      const notMoveSteps = 1;
-      move(+steps, notMoveSteps, pos);
-
-      return line;
     }
 
-    // TODO:
-    // Special case, H could possibly go around T so T won't move
-    // Maybe just re-calculate currentTailCoords without pushing to the store
-    if (+previousSteps === 1) {
-      return line;
-    }
-
-    // TODO: Calculate notMoveSteps for other cases (always > 0)
-    // For 'Back' case notMoveSteps = 2
-    // For 'Left' or 'Right' cases notMoveSteps = 1
-
-    // 'Back' case with 2 or more steps
-    if (backCases[previousDirection] === direction) {
-      const notMoveSteps = 2;
-      move(+steps, notMoveSteps, pos);
-
-      return line;
-    }
-
-    // Other usual cases
-    const notMoveSteps = 1;
-    move(+steps, notMoveSteps, pos);
-
-    return line;
-  }, 'R 0');
+    console.log(direction, steps);
+  });
 
   return store.getPositionsCount();
 }
-// Maybe handle cases wher there are 1 or 2 steps as special case: find a differend solurion
 
 console.log(getTailPositionsCount(lines));
