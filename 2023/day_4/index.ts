4a3f475174241069b3bdd1a7ee3a4d3cbe089360
Pavel-Surkov <surkovv.pavel@gmail.com>
2023-12-05 20:12:44 +0700

    feat: solve 2023/day_4 part one

---
+++ 2023/day_4/index.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+
+const input = fs.readFileSync(path.join(__dirname, './input.txt')).toString();
+
+function binarySearch<T>(value: T, arr: T[]) {
+  if (arr.length === 0) return false;
+
+  const centerIndex = Math.floor(arr.length / 2);
+
+  if (value === arr[centerIndex]) {
+    return true;
+  }
+
+  if (value > arr[centerIndex]) {
+    return binarySearch(value, arr.slice(centerIndex + 1));
+  } else {
+    return binarySearch(value, arr.slice(0, centerIndex));
+  }
+}
+
+const sumPoints = input
+  .split('\n')
+  .filter((line) => line !== '')
+  .map((line) =>
+    line
+      .split(/\:|\|/)
+      .slice(1)
+      .map((numbers) =>
+        numbers
+          .trim()
+          .split(' ')
+          .filter((number) => number !== '')
+          .map((n) => +n)
+      )
+  )
+  .reduce((sumPoints, numbers) => {
+    const [winningNumbers, sortedCardNumbers] = [
+      numbers[0],
+      numbers[1].sort((a, b) => a - b),
+    ];
+
+    let currentPoints = 0;
+
+    winningNumbers.forEach((num) => {
+      if (binarySearch(num, sortedCardNumbers)) {
+        currentPoints === 0 ? (currentPoints = 1) : (currentPoints *= 2);
+      }
+    });
+
+    return sumPoints + currentPoints;
+  }, 0);
+
+console.log(sumPoints);
