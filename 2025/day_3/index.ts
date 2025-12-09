ca8b0697ac893978c0f73f780c5ebd034ef5bf76
Surkov Pavel <ps@studiotribe.ru>
2025-12-10 02:01:31 +0700

    feat(2025): add day_3 part two solution

--- 2025/day_3/index.ts
+++ 2025/day_3/index.ts
@@ -30,10 +30,9 @@ function getMaxVoltageSum(lines: string[]) {
         }
       );
 
-    const maxUnitValue = batteryBank
-      .slice(maxDecIndex + 1, batteryBank.length)
-      .split('')
-      .reduce((max, current) => (max > +current ? max : +current), 0);
+    const maxUnitValue = findMaxNumberInString(
+      batteryBank.slice(maxDecIndex + 1, batteryBank.length)
+    );
 
     return sum + Number(`${maxDecValue}${maxUnitValue}`);
   }, 0);
@@ -41,3 +40,63 @@ function getMaxVoltageSum(lines: string[]) {
 
 console.log('Part one.');
 withTimeLog(getMaxVoltageSum, lines); // 17207
+
+// Part two
+// Теперь число состоит из 12-ти разрядов. Кастомизируем функцию под любое число разрядов
+// Создадим 2 указалетя — левый и правый. Ими будем выделять часть строки в которой
+// будем искать для каждого разряда максимальную цифру.
+// Далее соединим эти цифры в строку и преобразуем в число
+function getMaxVoltageSumV2(lines: string[], digitsCount: number) {
+  const finalSum = lines.reduce((sum, batteryBank) => {
+    const maxJoltageStr = recursivelyFindMaxJoltage(
+      batteryBank,
+      digitsCount,
+      0
+    );
+
+    return sum + Number(maxJoltageStr);
+  }, 0);
+
+  return finalSum;
+}
+
+console.log('Part two.');
+withTimeLog(getMaxVoltageSumV2, lines, 12); // 170997883706617
+
+// ------------------------------------------- Helper functions
+function findMaxNumberInString(bank: string) {
+  return bank
+    .split('')
+    .reduce((max, current) => (max > +current ? max : +current), 0);
+}
+
+function recursivelyFindMaxJoltage(
+  fullBank: string,
+  digitsLeft: number,
+  lpi: number = 0
+): string {
+  if (digitsLeft === 0) {
+    return '';
+  }
+
+  let rpi = fullBank.length - (digitsLeft - 1);
+
+  let nextLpi = lpi;
+  let maxNum = 0;
+
+  // Ищем макс цифру между указателями и ее индекс
+  for (let i = lpi; i < rpi; i++) {
+    const currentNum = +fullBank[i];
+
+    if (currentNum > maxNum) {
+      maxNum = currentNum;
+      nextLpi = i + 1;
+    }
+  }
+
+  return `${maxNum}${recursivelyFindMaxJoltage(
+    fullBank,
+    digitsLeft - 1,
+    nextLpi
+  )}`;
+}
